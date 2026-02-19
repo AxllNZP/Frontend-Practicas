ad986e6de83a2f2239f0edf6aa9c632cb0035868
AxllNZP <axellzurita1003@gmail.com>
2026-02-19 11:47:01 -0500

    UPDATE URL CONECTION

--- src/app/services/cliente.service.ts
+++ src/app/services/cliente.service.ts
@@ -7,7 +7,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry, tap } from 'rxjs/operators';
-import { environment } from '../environments/environment';
+import { environment } from '../environments/environment.prod';
 
 export interface ClienteDto {
   idCliente: number;
--- src/app/services/producto.service.ts
+++ src/app/services/producto.service.ts
@@ -7,8 +7,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry, tap } from 'rxjs/operators';
-import { environment } from '../environments/environment';
-
+import { environment } from '../environments/environment.prod';
 export type EstadoGeneral = 'activo' | 'inactivo';
 
 export interface ProductoDto {
